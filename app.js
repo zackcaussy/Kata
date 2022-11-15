40c555df3320fd2f4ecf62848705baa3ec9834d7
Zack Caussy <44547319+zackcaussy@users.noreply.github.com>
2022-11-15 16:04:41 +0100

    Update app.js

--- app.js
+++ app.js
@@ -241,3 +241,12 @@ function switchItUp(number){
   return "Nine";
   }
 }
+
+Invert values
+
+function invert(array) {
+  for(let i = 0 ; i < array.length; i++){
+    array[i] = array[i] * (-1) 
+  }
+  return array
+}
