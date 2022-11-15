7f12625530d66602a6fd358ff794c24170bb0f18
Zack Caussy <44547319+zackcaussy@users.noreply.github.com>
2022-11-15 09:31:36 +0100

    Update app.js

--- app.js
+++ app.js
@@ -150,3 +150,35 @@ function _if(bool, func1, func2) {
   }   
 }
 
+Quarter of the year
+
+const quarterOf = (month) => {
+  
+  switch(month)  {
+    case 1: 
+      return 1;
+    case 2:
+      return 1;
+    case 3:
+      return 1;
+    case 4:
+      return 2;
+    case 5:
+      return 2;
+    case 6:
+      return 2;
+    case 7:
+      return 3;
+    case 8:
+      return 3;
+    case 9:
+      return 3;
+    case 10:
+      return 4;
+    case 11:
+      return 4;
+    case 12:
+      return 4;
+ } 
+  return;
+}
