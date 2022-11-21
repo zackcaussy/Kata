38f5c1b4e8e93062cbe990479967f1431991c44e
Zack Caussy <44547319+zackcaussy@users.noreply.github.com>
2022-11-21 23:18:24 +0100

    Update app.js

--- app.js
+++ app.js
@@ -940,3 +940,34 @@ return +parseInt(bin, 2).toString(10);
 }
 }
 }
+   
+   
+   Get Planet Name By ID
+   
+   function getPlanetName(id){
+  var name;
+  switch(id){
+    case 1:
+      name = 'Mercury';
+      break;
+    case 2:
+      name = 'Venus';
+      break;
+    case 3:
+      name = 'Earth';
+      break;
+    case 4:
+      name = 'Mars';
+      break;
+    case 5:
+      name = 'Jupiter';
+      break;
+    case 6:
+      name = 'Saturn';
+      break;
+    case 7:
+      name = 'Uranus';
+      break;
+    case 8:
+      name = 'Neptune';
+      break;
