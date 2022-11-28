a82bb6d6fe9f13cc37fe49f238825a8497d8be7d
Zack Caussy <44547319+zackcaussy@users.noreply.github.com>
2022-11-28 22:44:50 +0100

    Update app.js

--- app.js
+++ app.js
@@ -995,3 +995,9 @@ Hex to Decimal
 function hexToDec(hexString){
 return parseInt(hexString, 16);
 }
+
+  A Needle in the Haystack  
+      
+      function findNeedle(haystack) {
+return "found the needle at position " + haystack.indexOf("needle"); 
+}
